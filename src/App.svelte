14451f1daf15a911346abe03360068fca39a8f8a
Walther Chen <walther.chen@gmail.com>
2020-05-24 15:55:36 -0400

    use onChange instead of bind

--- src/App.svelte
+++ src/App.svelte
@@ -21,46 +21,56 @@
 		["1-3", 66],
 	]);
 
-	// Just using two-way binding for selects. Should be fine for selects?
-	//Will it cause problems on larger projects?
-	let selected_country;
-	let selected_product_us;
-	let selected_product_ca;
+	// NOTE big note, `select` always returns a string. So if I want to use a number, `===` won't work.
+	// Maybe should just keep it as all strings.
+	let selected_country = 0;
+	let selected_product_us = 0;
+	let selected_product_ca = 2;
 
 
 	// have to use string as key; svelte cannot see into arrays.
+	// this should automatically update when selected is manually updated
 	$: us_time = time_map.get(selected_country + "-" + selected_product_us);
 	$: ca_time = time_map.get(selected_country + "-" + selected_product_ca);
 
+	function update_selected_country(value) {
+		selected_country = value;
+	}
+	function update_selected_product_us(value) {
+		selected_product_us = value;
+	}
+	function update_selected_product_ca(value) {
+		selected_product_ca = value;
+	}
 </script>
 
 <main>
 	<div>
-		<Select label="Country:" options={countries} bind:value={selected_country}>
+		<Select label="Country:" options={countries} value={selected_country} on_change="{ev => update_selected_country(ev.target.value)}">
 		</Select>
 		<p>Selected Country: {selected_country}</p>
 	</div>
-	{#if selected_country === 0 }
+	{#if selected_country == 0 }
 	<div>
-		<Select label="Product:" options={us_products} bind:value={selected_product_us}>
+		<Select label="Product:" options={us_products} value={selected_product_us} on_change="{ev => update_selected_product_us(ev.target.value)}">
 		</Select>
 		<p>Selected Product: {selected_product_us}</p>
 	</div>
-	{:else }
+	{:else if selected_country == 1}
 	<div>
-		<Select label="Product:" options={ca_products} bind:value={selected_product_ca}>
+		<Select label="Product:" options={ca_products} value={selected_product_ca} on_change="{ev => update_selected_product_ca(ev.target.value)}">
 		</Select>
 		<p>Selected Product: {selected_product_ca}</p>
 	</div>
 	{/if}
 
-	{#if selected_country === 0 }
+	{#if selected_country == 0 }
 	<div>
-		<p>Time: {us_time}</p>
+		<p>US product Time: {us_time}</p>
 	</div>
-	{:else }
+	{:else if selected_country == 2}
 	<div>
-		<p>Time: {ca_time}</p>
+		<p>CA product Time: {ca_time}</p>
 	</div>
 	{/if}
 </main>
--- src/Select.svelte
+++ src/Select.svelte
@@ -1,15 +1,13 @@
 <script>
 	export let label = "Default label";
 	export let options = [];
-
-	// undefined prevents warning about Component created with expected prop.
-	// Not sure the best way to handle this; right now I expect to bind to value prop, what happens when I don't?
 	export let value = undefined;
+	export let on_change = undefined;
 </script>
 
 <label>
 	{label}
-	<select bind:value={value}>
+	<select on:change={on_change}>
 		{#if options.length}
 			{#each options as option}
 				<option value={option.id} selected={option.id === value ? true : null}>
